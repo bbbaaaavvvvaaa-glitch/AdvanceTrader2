8ae5f081f082d509bd8866e13f4a234ab5fd5d28
bbbaaaavvvvaaa-glitch <bbbaaaavvvvaaa@gmail.com>
2025-11-22 16:45:00 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,44 @@
-const CACHE_NAME = "advance-trader-cache-v1";
+const CACHE_NAME = 'advance-tradar-2-cache-v1';
 const urlsToCache = [
-    "/",
-    "/index.html",
-    "/styles.css",
-    "/app.js",
-    "/manifest.json",
-    "/icons/icon.svg"
+    './index.html',
+    './styles.css',
+    './app.js',
+    './manifest.json',
+    './icons/icon.svg',
+    './alert.mp3'
 ];
 
-self.addEventListener("install", event => {
+// Install event
+self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(urlsToCache))
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-self.addEventListener("fetch", event => {
+// Activate event
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(name => {
+                    if (name !== CACHE_NAME) {
+                        return caches.delete(name);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-        .then(response => response || fetch(event.request))
+            .then(response => {
+                return response || fetch(event.request);
+            })
     );
 });
